1e27eff378efeb44d9f9b1c57bff3d214c8f1bd5
alexwaltz <92048497+alexwaltz@users.noreply.github.com>
2022-06-06 14:37:36 +0100

    install.sh

---
+++ install.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+sudo apt-get -y update
+
+sudo apt-get -y upgrade
+
+sudo apt install -y python3-tk
+
+sudo apt install -y python3-pip
+
+sudo apt-get install -y vim
+
+wget https://github.com/3rdIteration/btcrecover/archive/master.zip
+
+sudo apt-get install -y unzip
+
+sudo apt-get install -y autoconf automake build-essential libffi-dev libtool pkg-config python3-dev
+
+update-locale LANG=C.UTF-8
+
+apt install -y locales-all
+
+sudo apt install -y python3-pyopencl
+
+pip3 install coincurve~=15.0.1
+
+pip3 install green~=3.3.0
+
+pip3 install protobuf~=3.19.1
+
+pip3 install pycryptodome~=3.12.0
+
+pip3 install ecdsa==0.17.0
+
+pip3 install groestlcoin_hash~=1.0.3
+
+pip3 install eth-keyfile~=0.5.1
+
+pip3 install py_crypto_hd_wallet
+
+pip3 install pynacl~=1.4.0
+
+pip3 install bitstring~=3.1.9
+
+pip3 install shamir-mnemonic~=0.2.2
+
+pip3 install bitcoin-utils~=0.5.5
+
+unzip master.zip
+
